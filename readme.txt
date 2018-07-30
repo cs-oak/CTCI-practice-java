28e4093eb89aaf1782cd5d8f3990870c9c6e854e
Chirantan <chirantan722@gmail.com>
2018-07-30 19:53:02 -0400

    Initial commit / unique String question

---
+++ readme.txt
@@ -0,0 +1,4 @@
+Unfortunately, even after 2 years in the industry and 6 years in school, I am still an average programmer, if not below that.
+
+I only made this repo to practice CTCI questions for future interviews, and to let people know I know how to use git. The algos
+and problems I solve here are probably going to be far from perfect, but I appreciate any constructive criticism.
---
+++ src/UniqueString.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Implement an algorithm to determine if a given string has all unique characters
+ */
+
+public class UniqueString {
+
+    public boolean isUnique(String str) {
+        Set uniqueSet = new HashSet();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!uniqueSet.contains(str.charAt(i))) {
+                uniqueSet.add(str.charAt(i));
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void printUnqiue(boolean isUnique) {
+        System.out.println(isUnique ? "Unique!" : "Not Unique!");
+    }
+
+    public static void main(String[] args) {
+        UniqueString obj = new UniqueString();
+        final String UNIQUE = "python";
+        final String NOT_UNIQUE = "java";
+
+        obj.printUnqiue(obj.isUnique(UNIQUE));
+        obj.printUnqiue(obj.isUnique(NOT_UNIQUE));
+    }
+
+}
