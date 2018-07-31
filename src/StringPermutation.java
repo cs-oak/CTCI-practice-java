ac99e346ad5984e557053473959e615798f7ebc7
Chirantan Sharma <csharma@g.clemson.edu>
2018-07-30 20:39:06 -0400

    Added docs and some self confidence

--- src/StringPermutation.java
+++ src/StringPermutation.java
@@ -1,11 +1,25 @@
 import java.util.Arrays;
 
+/**
+ * Implement an algorithm to check if a string is a permutation of another
+ */
 public class StringPermutation {
 
+    /**
+     * Sort the strings and check if they're the same
+     * @param a
+     * @param b
+     * @return
+     */
     private boolean isPermutation(String a, String b) {
         return sortString(a).equals(sortString(b));
     }
 
+    /**
+     * Simple method to sort a String
+     * @param str
+     * @return
+     */
     private String sortString(String str) {
        char[] sort = str.toCharArray();
         Arrays.sort(sort);
