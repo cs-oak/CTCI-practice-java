ff6dba40e101c280cb137c688edc5d69832e7bc1
Chirantan Sharma <csharma@g.clemson.edu>
2018-09-30 20:14:31 -0400

    Reverse string class

---
+++ src/ReverseString.java
@@ -0,0 +1,28 @@
+public class ReverseString {
+
+    /**
+     * Implement an algorithm to reverse a string
+     */
+
+    private String reverseString(String input) {
+        char[] str = input.toCharArray();
+        int len = input.length() - 1;
+        char temp;
+        for (int i = 0; i <= len/2; i++) {
+            if (i == (len - i)) {
+                continue;
+            }
+
+            temp = str[i];
+            str[i] = str[len - i];
+            str[len - i] = temp;
+        }
+        return String.valueOf(str);
+    }
+
+    public static void main(String[] args) {
+        ReverseString obj = new ReverseString();
+        System.out.println(obj.reverseString("gnalog"));
+        System.out.println(obj.reverseString("12345"));
+    }
+}
