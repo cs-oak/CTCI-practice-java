57a721144ae31debfc7ef2ab27c3b750ffb9f8f8
Chirantan Sharma <csharma@g.clemson.edu>
2018-09-30 21:30:59 -0400

    String Compression

---
+++ src/CompressString.java
@@ -0,0 +1,37 @@
+public class CompressString {
+
+    /**
+     * Class to compress given string
+     */
+
+    private String compressString(String input){
+        StringBuilder out = new StringBuilder();
+        int count = 1;
+        char prev = input.charAt(0);
+
+        for (int i =1; i < input.length(); i++) {
+
+            if(input.charAt(i) == prev) {
+                count++;
+            } else {
+                out.append(prev);
+                out.append(count);
+                prev = input.charAt(i);
+                count = 1;
+            }
+        }
+
+        out.append(prev);
+        out.append(count);
+
+        String output = out.toString();
+        return (output.length() >= input.length()) ? input : output;
+    }
+
+    public static void main(String[] args) {
+        CompressString obj = new CompressString();
+        System.out.println(obj.compressString("aaabcccccaaa"));
+        System.out.println(obj.compressString("aabb"));
+        System.out.println(obj.compressString("Skyrim"));
+    }
+}
