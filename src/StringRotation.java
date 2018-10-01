9cd13069f9e1fc2e513b8389fc617f873bdc3190
Chirantan Sharma <csharma@g.clemson.edu>
2018-10-01 12:02:46 -0400

    String rotation

---
+++ src/StringRotation.java
@@ -0,0 +1,29 @@
+public class StringRotation {
+
+    /**
+     * Class to check if a given string is a rotation of a
+     * reference string by calling isSubstring method
+     */
+
+    private boolean isSubstring(String first, String second) {
+        return second.contains(first) || first.contains(second);
+    }
+
+    private boolean isRotation(String input, String reference) {
+        String str;
+        if (input.length() != reference.length()) {
+            return false;
+        } else {
+            str = input + input;
+            return isSubstring(str, reference);
+        }
+    }
+
+    public static void main(String[] args) {
+        StringRotation obj = new StringRotation();
+        System.out.println(obj.isRotation("erbottlewat", "waterbottle"));
+        System.out.println(obj.isRotation("clinteastwood", "woodclinteast"));
+        System.out.println(obj.isRotation("clinteastwood", "oldwestaction"));
+        System.out.println(obj.isRotation("dark souls", "i cannot parry"));
+    }
+}
